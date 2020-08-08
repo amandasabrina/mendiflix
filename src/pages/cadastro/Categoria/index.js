e842dc3db9ea71895bffcc08e29a1f4b82a29033
amandasabrina <amandasabrina_4@msn.com>
2020-08-08 18:07:05 -0300

    Updates

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -32,8 +32,11 @@ function CadastroCategoria() {
     }
 
     useEffect(() => {
-        console.log('alo alo');
-        const URL_TOP = 'http://localhost:8080/categorias';
+        // const URL_TOP = 'http://localhost:8080/categorias';
+        // \/ variavel de ambiente
+        const URL_TOP = window.location.hostname.includes('localhost') 
+        ? 'http://localhost:8080/categorias'
+        : 'https://mendiflix.herokuapp.com/categorias';
         fetch(URL_TOP)
             .then(async (respostaDoServidor) => {
                 const resposta = await respostaDoServidor.json();
